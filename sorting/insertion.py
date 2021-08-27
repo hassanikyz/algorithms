bed02d4dd2f2614ed5357bd806b0ab80edcb9340
hassanikyz <hassan.khan@gmail.com>
2021-08-27 07:45:58 -0700

    Update insertion.py

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -7,11 +7,11 @@ def insertionsort_iterative(intlist: list[int]) -> list[int]:
     if lenlist <= 1:
         return intlist
 
-    # look at the left side and find the right spot for the element on right (at ith location) 
-    # During the process left side is kept sorted at all times until we reach the end of the list 
+    # Look at the left side and find the correct spot for the element on the right (at ith location). 
+    # During the process left side is kept sorted at all times until we reach the end of the list. 
     # 0th location (left of 1st location) is assumed already sorted since that left side is composed
     # of only 1 element! Therefore we start with a 2nd element at location 1.   
-    # We compare our right element (on as part of outer loop) with each element on left side 
+    # We compare our right element (as part of outer loop) with each element on left side 
     # (as part of inner while loop) until we one which is smaller. 
 
     # In other words, during each comparison we see whether the element on the left is greater than the element on 
