c0bf49e490cb868d9106be8f45bc6cf40e1a737f
hassanikyz <hassan.khan@gmail.com>
2021-08-28 14:59:24 -0700

    Update selection.py
    Additional comments explaining the code

--- sorting/selection.py
+++ sorting/selection.py
@@ -12,9 +12,15 @@ def selection_sort(originalist: list[int]):
         minval_idx = i
         for j in range(i+1, listlen):
             if originalist[j] < originalist[minval_idx]:
+                #found a new min? Update the index
                 minval_idx = j
+
+        #pythonic way of swapping the first element of unsorted bunch with the newly found min value    
         originalist[i], originalist[minval_idx] = originalist[minval_idx], originalist[i]
 
     return originalist
 
 print(f"Calling selection sort {selection_sort([10, 7, 6, 4, 9, 3, 8, 5, 2, 0, 1])}")
+## output ###
+## Initial List is [10, 7, 6, 4, 9, 3, 8, 5, 2, 0, 1]
+## Calling selection sort [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
