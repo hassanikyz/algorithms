74d4c75a5e3bc9c6dec646ffe89ec013e9bd62f7
hassanikyz <hassan.khan@gmail.com>
2021-08-28 14:46:54 -0700

    Create selection.py
    Selection sort algorithm implementation in python

---
+++ sorting/selection.py
@@ -0,0 +1,20 @@
+def selection_sort(originalist: list[int]):
+    
+    listlen = len(originalist)
+
+    # Go through the entire list looking for a minimum value (smallest). Once min val is found swap it with
+    # the very first element of the unsorted elements.
+    # In the beginning the very first element is obviously at 0th location.
+    # However as min values are placed in their correct locations the size of the unsorted list shrinks
+    # and as a result the very first element of unsorted ones is now increasingly the next ith location.
+    
+    for i in range(listlen):
+        minval_idx = i
+        for j in range(i+1, listlen):
+            if originalist[j] < originalist[minval_idx]:
+                minval_idx = j
+        originalist[i], originalist[minval_idx] = originalist[minval_idx], originalist[i]
+
+    return originalist
+
+print(f"Calling selection sort {selection_sort([10, 7, 6, 4, 9, 3, 8, 5, 2, 0, 1])}")
