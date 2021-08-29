115ef20987a4175b586ed372b0548af1c231ad8d
hassanikyz <hassan.khan@gmail.com>
2021-08-28 18:50:29 -0700

    Update mergesort.py
    Additional explanations

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -9,16 +9,21 @@ def mergeBothLists(left: list[int], right: list[int]):
     # create an empty list which will have merged data from both right and left
     mergedList = []
 
+    # we don't know which one is shorter so ensuring we exit the loop whenever we hit last element of either of them.
     while (i < len(left) and j < len(right)):
+        
+      
         if (left[i] <= right[j]):
+            # if left has the smallest element take that and increment its counter
             mergedList.append( left[i])
             i += 1
-            
+
         elif left[i] > right[j]:
+            # else take the smallest from the right list and increment the counter
             mergedList.append(right[j])
             j += 1
         
-    # we don't know which of the two lists was shorter so best to take the remaining (if any) elements and simply 
+    # we don't know which of the two lists was shorter so best to take the remaining (if any) elements from both of them and simply 
     # add them to the mergedList to get the final output
     mergedList += left[i:]
     mergedList += right[j:]
