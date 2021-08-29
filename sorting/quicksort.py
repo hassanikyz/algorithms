20dc46acf597f846b822ce26c5c8272a8f21375f
hassanikyz <hassan.khan@gmail.com>
2021-08-28 21:46:34 -0700

    Create quicksort.py

---
+++ sorting/quicksort.py
@@ -0,0 +1,36 @@
+arr = [10, 7, 6, 4, 9, 3, 8, 5, 2, 0, 1]
+
+def partition(lst, left, right):
+
+    if (left >= right):
+        return lst
+
+    tmpval = lst[left]
+    i, j = left, right
+    while i <= j:
+
+        while (lst[i] < tmpval):
+            i += 1
+
+        while (lst[j] > tmpval):
+            j -= 1
+
+        if i <= j:
+            lst[i], lst[j] = lst[j], lst[i]
+            i +=1
+            j -=1
+
+    if (left < j):
+        partition(lst, left, j)
+    if (i < right):
+        partition(lst, i, right)
+
+    return lst
+
+
+
+def quicksort(lst: list[int]):
+    print(f"quicksort the list {lst}")
+    partition(lst, 0, len(lst) - 1)
+    print(f"sorted {lst}")
+    return lst
