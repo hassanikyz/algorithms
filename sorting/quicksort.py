21889fdafb3fbc3a9413c5baea7353f605e0988c
hassanikyz <hassan.khan@gmail.com>
2021-08-28 22:03:23 -0700

    Update quicksort.py

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -48,3 +48,10 @@ def quicksort(lst: list[int]):
     partition(lst, 0, len(lst) - 1)
     print(f"sorted {lst}")
     return lst
+
+
+### OUTPUT ####
+"""
+quicksort the list [10, 7, 6, 4, 9, 3, 8, 5, 2, 0, 1]
+sorted [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""
