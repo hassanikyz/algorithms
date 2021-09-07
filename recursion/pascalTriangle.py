b7b576ce7d41ea578197cb937d015a378454c13a
hassanikyz <hassan.khan@gmail.com>
2021-09-06 22:48:49 -0700

    Update pascalTriangle.py
    Explanatory comments

--- recursion/pascalTriangle.py
+++ recursion/pascalTriangle.py
@@ -6,6 +6,15 @@ def printPascal(s) :
     if s == 1:
       return [1, 1]
     
+    ## Using recursion to get the result of previous number and using that to build result for this number ##
+    ## Pascal Triangle is essentially 
+    ##                        1
+    ##                      1   1
+    ##                    1   2    1
+    ##                  1   3    3    1
+    ##                1   4    6    4    1
+    ##              1   5   10   10    5    1
+    
     slate = printPascal(s - 1)
     result = []
     result.append(1)
