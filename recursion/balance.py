fa6daf9dd27d431ca0153f5fe1458a76fd29fc40
hassanikyz <hassan.khan@gmail.com>
2021-09-07 10:56:24 -0700

    Create balance.py

---
+++ recursion/balance.py
@@ -0,0 +1,22 @@
+
+
+def balanced_iter(lst, startIndex = 0, currentIndex = 0) :
+
+  balance = 0
+
+  while balance >= 0 and currentIndex < len(lst):
+    while   currentIndex < len(lst) and lst[currentIndex] == '(' :
+      balance += 1 
+      currentIndex += 1
+    while  currentIndex < len(lst) and lst[currentIndex] == ')' :
+      balance -= 1
+      currentIndex += 1
+
+    if balance < 0:
+      return False
+
+  if balance < 0:
+      return False
+  return True
+
+print("Balanced is ", balanced([')', ')', '(', '(',  '(', ')']))
