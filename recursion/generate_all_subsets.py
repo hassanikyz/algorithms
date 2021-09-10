3367ee95eb1a0b98f1369281541f865ed931bdc4
hassanikyz <hassan.khan@gmail.com>
2021-09-10 10:51:00 -0700

    Update generate_all_subsets.py
    Explanatory comments

--- recursion/generate_all_subsets.py
+++ recursion/generate_all_subsets.py
@@ -2,9 +2,29 @@ def generate_all_subsets(s):
 
     def helper(slate, s, pos):
 
+        """
+        Generating all subsets mean we explore all possible combinations. Going through the input we have two options when creating a set
+        1. Include the element
+        2. Not include the element
+        
+        E.g "abc"
+        We don't include anything we get empty set
+        ""
+        We include just the first element
+        "a"
+        Using these two sets as base we can now repeat the same process with next element in the input
+        "" + "b" => "b"
+        and
+        "a" + "b"  => "ab" 
+        Similarly for the 3rd input 'c' we have previous 4 sets to use as base and create 4 more sets by adding 3 to each
+        
+        
+        
+        
+        """
+        
         if (pos >= len(s)):
             res.append("".join(slate))
-            print("".join(slate))
         else:
     
             if len(slate) == 0 or s[pos] not in slate:
